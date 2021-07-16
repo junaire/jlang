524ddbc0a6bd7bc0f3099866f0cf2b38cc727f95
Jun <jun@junz.org>
2021-07-16 11:38:42 +0800

    add AST

--- jlang.cpp
+++ jlang.cpp
@@ -75,3 +75,64 @@ static int gettok() {
 
 	return ThisChar;
 }
+
+class ExprAST {
+	public:
+		virtual ~ExprAST() = 0;
+};
+
+class NumberExprAST : public ExprAST {
+	public:
+		NumberExprAST(double v) : Val(v) {}
+	private:
+		double Val;
+};
+
+class VariableExprAST : public ExprAST {
+	public:
+		VariableExprAST(const std::string& str) : Name(str) {}
+	private:
+		std::string Name;
+};
+
+class BinaryExprAST : public ExprAST {
+	public:
+		BinaryExprAST(char op, 
+				std::unique_ptr<ExprAST> lhs, 
+				std::unique_ptr<ExprAST> rhs) :
+			Op(op), LHS(std::move(lhs)), RHS(std::move(rhs)) {}
+	private:
+		char Op;
+		std::unique_ptr<ExprAST> LHS, RHS;
+};
+
+
+class CallExprAST : public ExprAST {
+	public:
+		CallExprAST(const std::string& callee, 
+				std::vector<std::unique_ptr<ExprAST>> args) :
+			Callee(callee), Args(std::move(args)) {}
+	private:
+		std::string Callee;
+		std::vector<std::unique_ptr<ExprAST>> Args;
+};
+
+class PrototypeAST {
+	public:
+		PrototypeAST(const std::string& name, std::vector<std::string> args) :
+			Name(name), Args(args) {}
+		const std::string& getName() const { return this->Name; }
+	private:
+		std::string Name;
+		std::vector<std::string> Args;
+};
+
+class FunctionAST {
+	public:
+		FunctionAST(std::unique_ptr<PrototypeAST> proto, std::unique_ptr<ExprAST> body) :
+			Proto(std::move(proto)), Body(std::move(body)) {}
+	private:
+		std::unique_ptr<PrototypeAST> Proto;
+		std::unique_ptr<ExprAST> Body;
+};
+
